4bb4ff57e7e9f4bf9445ce6e90d0c28bae8fb8e8
Joey Sacchini <joey.sacchini264@gmail.com>
2016-12-16 13:10:29 -0500

    added documentation, a few options to toggle, and reformatted using gofmt

--- file/file.go
+++ file/file.go
@@ -1,198 +1,268 @@
 package file
 
 import (
-  "bytes"
-  "time"
-  "io"
-  "compress/gzip"
-  "fmt"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"math/rand"
+	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// The header of a saved file
+// This contains the essential metadata that's encoded in the first 50 (or so) bytes of a file
 type FileHeader struct {
-  UploadDate time.Time
-  ApiKeyID uint8
-  MimeType, OriginalName string
+	// The exact time that the webserver received the upload
+	UploadDate time.Time
+	// The index of the API Key which was used to do this upload.
+	// This is only really useful to the application which has the array of API keys
+	ApiKeyID uint8
+	// the mimetype and original name of the file for the browser.
+	// These are simply provided values, and may not accurately reflect reality
+	MimeType, OriginalName string
 }
 
+// A file that has been opened and can be read.
+// Contains a read header, and privately contains a reader which will decompress
 type FileHandle struct {
-  Header FileHeader
-  source *gzip.Reader
-  rawSource io.Reader
+	// The loaded header
+	Header FileHeader
+	// This could be a decompressed reader, or it could be nil.
+	// It's set when SetReadMode is called with DecompressReadMode
+	decompressSource *gzip.Reader
+	// The reader for the remainder of the file. This is the raw compressed data from the file.
+	rawSource io.Reader
+	// The source we're currently reading from. This points to either rawSource or decompressSource
+	currentSource io.Reader
 }
 
+type ReadMode uint8
+
+const (
+	DecompressReadMode ReadMode = iota
+	RawReadMode
+)
+
+// Set the read mode of the file to either RawReadMode or DecompressReadMode
+// if you set it to DecompressReadMode, the file will be decompressed and the handle will read bytes that are from the original upload
+// if you set it to RawReadMode, the handle will return the raw gziped bytes from the encoded file
+func (handle *FileHandle) SetReadMode(mode ReadMode) {
+	switch mode {
+	case DecompressReadMode:
+		if handle.decompressSource == nil {
+			decompress, err := gzip.NewReader(handle.rawSource)
+			if err != nil {
+				panic(err)
+			}
+			handle.decompressSource = decompress
+		}
+		handle.currentSource = handle.decompressSource
+	case RawReadMode:
+		handle.currentSource = handle.rawSource
+	default:
+		panic("bad mode passed")
+	}
+}
+
+// Closes the readers and performs other i/o cleanup
 func (handle *FileHandle) Close() error {
-  handle.source.Close()
-  if cls, ok := handle.rawSource.(io.Closer); ok {
-    return cls.Close()
-  }
-  return nil
+	handle.decompressSource.Close()
+	if cls, ok := handle.rawSource.(io.Closer); ok {
+		return cls.Close()
+	}
+	handle.currentSource = nil
+	return nil
 }
 
+// This will read bytes into the passed array, returning the number of bytes read and or an error
+// this implements io.Reader
 func (handle *FileHandle) Read(bytes []byte) (n int, err error) {
-  return handle.source.Read(bytes)
+	return handle.currentSource.Read(bytes)
 }
 
+// This will write into the writer, all of the bytes from this file based on SetReadMode
+// this implements io.WriterTo
 func (handle *FileHandle) WriteTo(w io.Writer) (n int64, err error) {
-  defer handle.Close()
-  return io.Copy(w, handle.source)
+	defer handle.Close()
+	return io.Copy(w, handle)
 }
 
+// Writes to the provided writer, using metadata from the "FileHeader" using the file contained in
+// the io.Reader src; so the call is something like this -> WriteFile(destination, info, sourceFile)
+//
+// Returns the number of bytes written, or an error
 func WriteFile(dst io.Writer, info FileHeader, src io.Reader) (nWritten int, err error) {
-  if dst == nil || src == nil {
-    err = fmt.Errorf("you must provide a non nil dst and src")
-    return
-  }
-  n, err := dst.Write([]byte{0xFA, 0xFA})
-  if err != nil {
-    return
-  }
-  nWritten += n
+	if dst == nil || src == nil {
+		err = fmt.Errorf("you must provide a non nil dst and src")
+		return
+	}
+	n, err := dst.Write([]byte{0xFA, 0xFA})
+	if err != nil {
+		return
+	}
+	nWritten += n
 
-  var headerBuffer bytes.Buffer
-  _, err = writeNumber(uint(info.UploadDate.UnixNano() / 10E8), 5, &headerBuffer)
-  if err != nil {
-    return
-  }
-  err = headerBuffer.WriteByte(byte(info.ApiKeyID))
-  if err != nil {
-    return
-  }
-  _, err = writeLengthPrefixedString(info.MimeType, &headerBuffer)
-  if err != nil {
-    return
-  }
-  _, err = writeLengthPrefixedString(info.OriginalName, &headerBuffer)
-  if err != nil {
-    return
-  }
+	var headerBuffer bytes.Buffer
+	_, err = writeNumber(uint(info.UploadDate.UnixNano()/10E8), 5, &headerBuffer)
+	if err != nil {
+		return
+	}
+	err = headerBuffer.WriteByte(byte(info.ApiKeyID))
+	if err != nil {
+		return
+	}
+	_, err = writeLengthPrefixedString(info.MimeType, &headerBuffer)
+	if err != nil {
+		return
+	}
+	_, err = writeLengthPrefixedString(info.OriginalName, &headerBuffer)
+	if err != nil {
+		return
+	}
 
-  headLength := headerBuffer.Len()
-  n64, err := writeNumber(uint(headLength), 2, dst)
-  if err != nil {
-    return
-  }
-  nWritten += int(n64)
-  n64, err = headerBuffer.WriteTo(dst)
-  if err != nil {
-    return
-  }
-  nWritten += int(n64)
-  if err != nil {
-    return
-  }
+	headLength := headerBuffer.Len()
+	n64, err := writeNumber(uint(headLength), 2, dst)
+	if err != nil {
+		return
+	}
+	nWritten += int(n64)
+	n64, err = headerBuffer.WriteTo(dst)
+	if err != nil {
+		return
+	}
+	nWritten += int(n64)
+	if err != nil {
+		return
+	}
 
-  compressionWriter := gzip.NewWriter(dst)
-  defer compressionWriter.Close()
-  defer compressionWriter.Flush()
-  n64, err = io.Copy(compressionWriter, src)
-  if err != nil {
-    return
-  }
-  nWritten += int(n64)
-  return
+	compressionWriter := gzip.NewWriter(dst)
+	defer compressionWriter.Close()
+	defer compressionWriter.Flush()
+	n64, err = io.Copy(compressionWriter, src)
+	if err != nil {
+		return
+	}
+	nWritten += int(n64)
+	return
 }
 
+// Reads just the header from a source, and returns it in the struct, as well as the number of bytes read, and the error
 func ReadFileHeader(src io.Reader) (header FileHeader, nRead int, err error) {
-  twoBytes := make([]byte, 2)
-  n, err := src.Read(twoBytes)
-  if err != nil {
-    return
-  }
-  nRead += n
-  if twoBytes[0] != twoBytes[1] && twoBytes[0] != 0xFA {
-    err = fmt.Errorf("file does not begin with header 0xFAFA")
-    return
-  }
+	twoBytes := make([]byte, 2)
+	n, err := src.Read(twoBytes)
+	if err != nil {
+		return
+	}
+	nRead += n
+	if twoBytes[0] != twoBytes[1] && twoBytes[0] != 0xFA {
+		err = fmt.Errorf("file does not begin with header 0xFAFA")
+		return
+	}
 
-  headerLength, err := readNumber(2, src)
-  if err != nil {
-    return
-  }
-  headerBytes := make([]byte, headerLength)
+	headerLength, err := readNumber(2, src)
+	if err != nil {
+		return
+	}
+	headerBytes := make([]byte, headerLength)
 
-  n, err = src.Read(headerBytes)
-  if err != nil {
-    return
-  }
+	n, err = src.Read(headerBytes)
+	if err != nil {
+		return
+	}
 
-  headerBuf := bytes.NewBuffer(headerBytes)
-  secondsTime, err := readNumber(5, headerBuf)
-  if err != nil {
-    return
-  }
-  header.UploadDate = time.Unix(int64(secondsTime), 0)
-  header.ApiKeyID, err = headerBuf.ReadByte()
-  if err != nil {
-    return
-  }
-  header.MimeType, _, err = readLengthPrefixedString(headerBuf)
-  if err != nil {
-    return
-  }
-  header.OriginalName, _, err = readLengthPrefixedString(headerBuf)
-  if err != nil {
-    return
-  }
-  return
+	headerBuf := bytes.NewBuffer(headerBytes)
+	secondsTime, err := readNumber(5, headerBuf)
+	if err != nil {
+		return
+	}
+	header.UploadDate = time.Unix(int64(secondsTime), 0)
+	header.ApiKeyID, err = headerBuf.ReadByte()
+	if err != nil {
+		return
+	}
+	header.MimeType, _, err = readLengthPrefixedString(headerBuf)
+	if err != nil {
+		return
+	}
+	header.OriginalName, _, err = readLengthPrefixedString(headerBuf)
+	if err != nil {
+		return
+	}
+	return
 }
 
+// Reads from the reader, into a FileHandle, which can be used to read the entire file.
+// May return an error
 func ReadFile(src io.Reader) (handle *FileHandle, err error) {
-  if src == nil {
-    err = fmt.Errorf("you need to provide a non nil reader")
-    return
-  }
-  handle = &FileHandle{rawSource: src}
-  handle.Header, _, err = ReadFileHeader(src)
-  if err == nil {
-    handle.source, err = gzip.NewReader(src)
-    if err != nil {
-      return
-    }
-  }
+	if src == nil {
+		err = fmt.Errorf("you need to provide a non nil reader")
+		return
+	}
+	handle = &FileHandle{rawSource: src}
+	handle.Header, _, err = ReadFileHeader(src)
+	if err != nil {
+		return
+	}
+	handle.SetReadMode(RawReadMode)
 
-  return
+	return
+}
+
+const charset = "abcdefghkmnoprstwxzABCDEFGHJKLMNPQRTWXY34689"
+
+// Returns a random string that's good for human URLs.
+func CreateRandomFileName(length int) string {
+	if length <= 0 {
+		return ""
+	}
+
+	return string([]rune(charset)[rand.Intn(len(charset))]) + CreateRandomFileName(length-1)
 }
 
 func writeNumber(number uint, byteCount int, out io.Writer) (n int64, err error) {
-  var buffer bytes.Buffer
-  for i := 0; i < byteCount; i++ {
-    shift := uint(8 * (byteCount - i - 1))
-    buffer.WriteByte(byte(number >> shift))
-  }
-  return buffer.WriteTo(out)
+	var buffer bytes.Buffer
+	for i := 0; i < byteCount; i++ {
+		shift := uint(8 * (byteCount - i - 1))
+		buffer.WriteByte(byte(number >> shift))
+	}
+	return buffer.WriteTo(out)
 }
 
 func readNumber(byteCount int, in io.Reader) (number uint64, err error) {
-  bytez := make([]byte, byteCount)
-  _, err = in.Read(bytez)
-  if err != nil {
-    return
-  }
-  for i := 0; i < byteCount; i++ {
-    shift := uint(8 * (byteCount - i - 1))
-    number = number | uint64(bytez[i]) << shift
-  }
-  return
+	bytez := make([]byte, byteCount)
+	_, err = in.Read(bytez)
+	if err != nil {
+		return
+	}
+	for i := 0; i < byteCount; i++ {
+		shift := uint(8 * (byteCount - i - 1))
+		number = number | uint64(bytez[i])<<shift
+	}
+	return
 }
 
 func writeLengthPrefixedString(str string, out io.Writer) (n int64, err error) {
-  var buffer bytes.Buffer
-  writeNumber(uint(len(str)), 2, &buffer)
-  buffer.WriteString(str)
-  return buffer.WriteTo(out)
+	var buffer bytes.Buffer
+	writeNumber(uint(len(str)), 2, &buffer)
+	buffer.WriteString(str)
+	return buffer.WriteTo(out)
 }
 
 func readLengthPrefixedString(in io.Reader) (str string, n int64, err error) {
-  length, err := readNumber(2, in)
-  if err != nil {
-    return
-  }
-  stringBytes := make([]byte, length)
-  _, err = in.Read(stringBytes)
-  if err != nil {
-    return
-  }
-  str = string(stringBytes)
-  n = 2 + int64(length)
-  return
+	length, err := readNumber(2, in)
+	if err != nil {
+		return
+	}
+	stringBytes := make([]byte, length)
+	_, err = in.Read(stringBytes)
+	if err != nil {
+		return
+	}
+	str = string(stringBytes)
+	n = 2 + int64(length)
+	return
 }
--- file/file_test.go
+++ file/file_test.go
@@ -1,135 +1,174 @@
 package file
 
 import (
-  "testing"
-  "os"
-  "io"
-  "io/ioutil"
-  "bytes"
-  "time"
-  "fmt"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"testing"
+	"time"
 )
 
 var testFileHeader = FileHeader{time.Unix(1481834107, 0), uint8(1), "image/png", "Screenshot at todo"}
 
 func TestRead(t *testing.T) {
-  target, err := os.Open("test_data/encoded")
-  if err != nil {
-    t.Error(err)
-  }
-  handle, err := ReadFile(target)
+	target, err := os.Open("test_data/encoded")
+	if err != nil {
+		t.Error(err)
+	}
+	handle, err := ReadFile(target)
+	if err != nil {
+		t.Error(err)
+	}
+	handle.SetReadMode(DecompressReadMode)
 
-  header := handle.Header
-  if header != testFileHeader {
-    t.Error("header does not match")
-  }
-  checkAgainstReal(t, handle, "raw")
+	header := handle.Header
+	if header != testFileHeader {
+		t.Error("header does not match")
+	}
+	checkAgainstReal(t, handle, "raw")
 }
 
 func TestWrite(t *testing.T) {
-  var buffer bytes.Buffer
-  source, err := os.Open("test_data/raw")
-  _, err = WriteFile(&buffer, testFileHeader, source)
-  if err != nil {
-    t.Error(err)
-  }
-  checkAgainstReal(t, &buffer, "encoded")
+	var buffer bytes.Buffer
+	source, err := os.Open("test_data/raw")
+	_, err = WriteFile(&buffer, testFileHeader, source)
+	if err != nil {
+		t.Error(err)
+	}
+	checkAgainstReal(t, &buffer, "encoded")
 }
 
 func TestBadRead(t *testing.T) {
-  badData := make([]byte, 128)
-  _, err := ReadFile(bytes.NewReader(badData))
-  if err == nil {
-    t.Error("there was no error... the data was clearly bad, but we had no error.")
-  }
+	badData := make([]byte, 128)
+	_, err := ReadFile(bytes.NewReader(badData))
+	if err == nil {
+		t.Error("there was no error... the data was clearly bad, but we had no error.")
+	}
 
-  _, err = ReadFile(nil)
-  if err == nil {
-    t.Error("there was no error when a nil writer was provided")
-  }
+	_, err = ReadFile(nil)
+	if err == nil {
+		t.Error("there was no error when a nil writer was provided")
+	}
 }
 
 func TestBadWrite(t *testing.T) {
-  _, err := WriteFile(nil, FileHeader{}, nil)
-  if err == nil {
-    t.Error("there was no error when a nil src/dst was provided to the Write function")
-  }
+	_, err := WriteFile(nil, FileHeader{}, nil)
+	if err == nil {
+		t.Error("there was no error when a nil src/dst was provided to the Write function")
+	}
+}
+
+func TestRandomGenerating(t *testing.T) {
+	randomLong := CreateRandomFileName(32)
+	alsoRandomLong := CreateRandomFileName(32)
+	randomShort := CreateRandomFileName(8)
+	alsoRandomShort := CreateRandomFileName(8)
+	bad := CreateRandomFileName(-1)
+	patternLong := regexp.MustCompile("^[" + charset + "]{32}$")
+	patternShort := regexp.MustCompile("^[" + charset + "]{8}$")
+
+	if randomLong == alsoRandomLong || randomShort == alsoRandomShort {
+		t.Error("The generated strings are not unique", randomLong, randomShort)
+	}
+
+	if !patternLong.MatchString(randomLong) || !patternLong.MatchString(alsoRandomLong) {
+		t.Error("The generated long strings do not match the regex", randomLong, alsoRandomLong)
+	}
+
+	if !patternShort.MatchString(randomShort) || !patternShort.MatchString(alsoRandomShort) {
+		t.Error("The generated short strings do not match their regex", randomShort, alsoRandomShort)
+	}
+
+	if len(bad) > 0 {
+		t.Error("The function returned a value when passed -1:", bad)
+	}
 }
 
 func BenchmarkWrite(b *testing.B) {
-  encodedInfo, err := os.Stat("test_data/encoded")
-  if err != nil {
-    return
-  }
-  encodedSize := int(encodedInfo.Size())
+	encodedInfo, err := os.Stat("test_data/encoded")
+	if err != nil {
+		return
+	}
+	encodedSize := int(encodedInfo.Size())
 
-  source, err := ioutil.ReadFile("test_data/raw")
-  if err != nil {
-    b.Error(err)
-  }
-  b.ResetTimer()
-  for i := 0; i < b.N; i++ {
-    var buffer bytes.Buffer
-    buffer.Grow(encodedSize)
-    WriteFile(&buffer, testFileHeader, bytes.NewReader(source))
-  }
+	source, err := ioutil.ReadFile("test_data/raw")
+	if err != nil {
+		b.Error(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buffer bytes.Buffer
+		buffer.Grow(encodedSize)
+		WriteFile(&buffer, testFileHeader, bytes.NewReader(source))
+	}
 }
 
 func BenchmarkRead(b *testing.B) {
-  rawInfo, err := os.Stat("test_data/raw")
-  if err != nil {
-    return
-  }
-  rawSize := int(rawInfo.Size())
+	rawInfo, err := os.Stat("test_data/raw")
+	if err != nil {
+		return
+	}
+	rawSize := int(rawInfo.Size())
 
-  source, err := ioutil.ReadFile("test_data/encoded")
-  if err != nil {
-    b.Error(err)
-  }
+	source, err := ioutil.ReadFile("test_data/encoded")
+	if err != nil {
+		b.Error(err)
+	}
 
-  b.ResetTimer()
-  for i := 0; i < b.N; i++ {
-    handle, err := ReadFile(bytes.NewReader(source))
-    if err != nil {
-      b.Error(err)
-    }
-    data, err := ioutil.ReadAll(handle)
-    if err != nil {
-      b.Error(err)
-    }
-    if len(data) != rawSize {
-      b.Error("the data was not a valid size...")
-    }
-  }
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		handle, err := ReadFile(bytes.NewReader(source))
+		if err != nil {
+			b.Error(err)
+		}
+		handle.SetReadMode(DecompressReadMode)
+		data, err := ioutil.ReadAll(handle)
+		if err != nil {
+			b.Error(err)
+		}
+		if len(data) != rawSize {
+			b.Error("the data was not a valid size...")
+		}
+		handle.Close()
+	}
+}
+
+func BenchmarkRandomStringLen9(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CreateRandomFileName(9)
+	}
 }
 
 func checkAgainstReal(t *testing.T, handle io.Reader, real string) {
-  realData, err := ioutil.ReadFile("test_data/" + real)
-  if err != nil {
-    t.Error(err)
-  }
-  ourData, err := ioutil.ReadAll(handle)
-  if err != nil {
-    t.Error(err)
-  }
+	realData, err := ioutil.ReadFile("test_data/" + real)
+	if err != nil {
+		t.Error(err)
+	}
+	ourData, err := ioutil.ReadAll(handle)
+	if err != nil {
+		t.Error(err)
+	}
 
-  if !bytes.Equal(realData, ourData) {
-    var iS int
-    for i := 0; i < localMin(len(realData), len(ourData)); i++ {
-      if realData[i] == ourData[i] {
-        continue
-      }
-      iS = i
-      break
-    }
-    fmt.Printf("Inequality at %#x\n\t%v\n\t%v\n", iS, realData[iS:localMin(len(realData), iS + 20)], ourData[iS:localMin(len(ourData), iS + 20)])
-    t.Error("equality check failed when comparing generated to test_data/" + real)
-  }
+	if !bytes.Equal(realData, ourData) {
+		var iS int
+		for i := 0; i < localMin(len(realData), len(ourData)); i++ {
+			if realData[i] == ourData[i] {
+				continue
+			}
+			iS = i
+			break
+		}
+		fmt.Printf("Inequality at %#x\n\t%v\n\t%v\n", iS, realData[iS:localMin(len(realData), iS+20)], ourData[iS:localMin(len(ourData), iS+20)])
+		t.Error("equality check failed when comparing generated to test_data/" + real)
+	}
 }
 
 func localMin(a, b int) int {
-  if a < b {
-    return a
-  }
-  return b
+	if a < b {
+		return a
+	}
+	return b
 }
